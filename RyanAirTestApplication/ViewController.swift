4cdc82f3d228166e853bba593b832985c90f0e8d
Sos Avetyan <sosa@arebus.io>
2021-05-25 16:26:44 +0400

    Initial Commit

---
+++ RyanAirTestApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RyanAirTestApplication
+//
+//  Created by Sos Avetyan on 5/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
